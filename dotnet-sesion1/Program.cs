6a7b4bb74016c0b7ec2cba0bf9fbd6e7fb966ff3
fred <fduartej@gmail.com>
2019-08-10 12:21:30 -0500

    First Commit

---
+++ dotnet-sesion1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace dotnet_sesion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                      
+            String name;
+            String password;
+            int edad;
+            
+            Console.WriteLine("My App! ");
+            Console.WriteLine("Ingrese su nombre: ");
+            name=Console.ReadLine();
+            Console.WriteLine("Ingrese su password: ");
+            password=Console.ReadLine();
+            Console.WriteLine("Ingrese su edad: ");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello {0}",name);
+            if(edad>18){
+               Console.WriteLine("Puede Beber!");     
+            }else{
+               Console.WriteLine("No debe Beber!");      
+            }
+        }
+    }
+}
