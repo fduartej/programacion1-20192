f51d6d84105cd30c86e95995b3b0d98f3231ac82
Fred <fduartej@gmail.com>
2019-08-17 13:35:41 -0500

    Not Working

--- dotnet-web/Controllers/HomeController.cs
+++ dotnet-web/Controllers/HomeController.cs
@@ -7,11 +7,14 @@ namespace dotnet_web.controllers
 
     public class HomeController:Controller{
 
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
         public IActionResult Index(){
             return View();
         }
         
-        [HttpPost]
+        [HttpPost("/RegistrarFan")]
         public IActionResult RegistrarFan(Follower follow){
             int age =DateTime.Now.Year - follow.Birth.Year;
             return View("Myview");
